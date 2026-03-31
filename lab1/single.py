ce99172aafdcb73cb8a754f581dc85feb290f536
YanchenDing <dingyanchen2005@gmail.com>
2026-03-31 17:22:35 +0800

    user interaction; dynamics

--- lab1/single.py
+++ lab1/single.py
@@ -37,6 +37,9 @@ class RigidBody:
     ):
         raise NotImplementedError
 
+    def ray_intersect(self, orig_l, dir_l):
+        raise NotImplementedError
+
 
 @ti.data_oriented
 class Cuboid(RigidBody):
@@ -60,19 +63,15 @@ class Cuboid(RigidBody):
         self.size = np.array(cfg["size"], dtype=np.float32)
         self.half_extent = 0.5 * self.size
 
-    def cuboid_inertia_diag(m, size):
-        lx, ly, lz = size
-        ixx = (m / 12.0) * (ly * ly + lz * lz)
-        iyy = (m / 12.0) * (lx * lx + lz * lz)
-        izz = (m / 12.0) * (lx * lx + ly * ly)
-        return np.array([ixx, iyy, izz], dtype=np.float32)
-
     def get_inertia_diag(self):
         lx, ly, lz = self.size
         ixx = (self.mass / 12.0) * (ly * ly + lz * lz)
         iyy = (self.mass / 12.0) * (lx * lx + lz * lz)
         izz = (self.mass / 12.0) * (lx * lx + ly * ly)
         return np.array([ixx, iyy, izz], dtype=np.float32)
+
+    def ray_intersect(self, orig_l, dir_l):
+        return ray_aabb_intersect(orig_l, dir_l, self.half_extent)
 
     def setup_mesh(self, indices_field, i_offset, v_offset):
         self._setup_mesh(indices_field, i_offset, v_offset)
@@ -186,20 +185,19 @@ class Scene:
         self.update_mesh_vertices()
 
     def get_state(self):
-        return {
-            "position": self.position.to_numpy().copy(),
-            "velocity": self.velocity.to_numpy().copy(),
-            "rotation": self.rotation.to_numpy().copy(),
-            "angular_velocity": self.angular_velocity.to_numpy().copy(),
-        }
+        pos = self.position.to_numpy().copy()
+        vel = self.velocity.to_numpy().copy()
+        rot = self.rotation.to_numpy().copy()
+        ang_vel = self.angular_velocity.to_numpy().copy()
+        return pos, vel, rot, ang_vel
 
-    def set_state(self, state):
+    def set_state(self, pos, vel, rot, ang_vel):
         n = self.num_bodies[None]
         for i in range(n):
-            self.position[i] = state["position"][i]
-            self.velocity[i] = state["velocity"][i]
-            self.rotation[i] = state["rotation"][i]
-            self.angular_velocity[i] = state["angular_velocity"][i]
+            self.position[i] = pos[i]
+            self.velocity[i] = vel[i]
+            self.rotation[i] = rot[i]
+            self.angular_velocity[i] = ang_vel[i]
 
     def update_mesh_vertices(self):
         v_offset = 0
@@ -213,6 +211,10 @@ class Scene:
 class Simulator:
     def __init__(self, scene: Scene):
         self.scene = scene
+        self.masses = self.scene.mass.to_numpy()
+        self.inv_inertia = self.scene.inv_inertia_body.to_numpy()
+        self.inertia = self.scene.inertia_body.to_numpy()
+        self.n_bodies = self.scene.num_bodies[None]
 
     def render(self, window, camera, canvas, scene_3d):
         scene_3d.set_camera(camera)
@@ -230,36 +232,43 @@ class Simulator:
         window.get_canvas().set_background_color((0.8, 0.8, 0.85))
         window.show()
 
-    def step(self):
+    def step(self, applied_forces=None, applied_torques=None):
         substeps = self.scene.substeps
         dt = self.scene.dt / substeps
-
-        state = self.scene.get_state()
-        pos = state["position"]
-        vel = state["velocity"]
-        rot = state["rotation"]
-        ang_vel = state["angular_velocity"]
+        pos, vel, rot, ang_vel = self.scene.get_state()
 
         for _ in range(substeps):
+            if applied_forces is not None:
+                vel += dt * (applied_forces / self.masses[:, None])
             vel += dt * self.scene.gravity[None, :]
             vel *= max(0.0, 1.0 - self.scene.linear_damping * dt)
+
+            for i in range(self.n_bodies):
+                R = rot[i]
+                I_inv = R @ self.inv_inertia[i] @ R.T
+                I_curr = R @ self.inertia[i] @ R.T
+
+                tau = (
+                    applied_torques[i]
+                    if applied_torques is not None
+                    else np.zeros(3, dtype=np.float32)
+                )
+                omega = ang_vel[i]
+
+                # Including the gyroscopic term: tau - omega x (I * omega)
+                tau_total = tau - np.cross(omega, I_curr @ omega)
+                ang_vel[i] += dt * (I_inv @ tau_total)
+
             ang_vel *= max(0.0, 1.0 - self.scene.angular_damping * dt)
             pos += dt * vel
 
-            for i in range(self.scene.num_bodies[None]):
+            for i in range(self.n_bodies):
                 rot[i] = self._integrate_rotation(rot[i], ang_vel[i], dt)
 
-        self.scene.set_state(
-            {
-                "position": pos,
-                "velocity": vel,
-                "rotation": rot,
-                "angular_velocity": ang_vel,
-            }
-        )
+        self.scene.set_state(pos, vel, rot, ang_vel)
         self.scene.update_mesh_vertices()
 
-    def _integrate_rotation(r, omega, dt):
+    def _integrate_rotation(self, r, omega, dt):
         dtheta = omega * dt
         theta = np.linalg.norm(dtheta)
         if theta < 1e-7:
@@ -283,9 +292,90 @@ class Simulator:
         camera.lookat(0, 0.5, 0)
         camera.up(0, 1, 0)
 
+        selected_body = -1
+        click_pos_w = None
+        click_normal_w = None
+        original_mouse_pos = None
+
         frame = 0
         while window.running and frame < (steps if steps > 0 else float("inf")):
-            self.step()
+            ctrl_pressed = window.is_pressed(ti.ui.CTRL)
+            lmb_pressed = window.is_pressed(ti.ui.LMB)
+            mouse_pos = window.get_cursor_pos()
+
+            if not ctrl_pressed:
+                camera.track_user_inputs(
+                    window, movement_speed=0.03, hold_key=ti.ui.RMB
+                )
+
+            applied_forces = np.zeros((self.n_bodies, 3), dtype=np.float32)
+            applied_torques = np.zeros((self.n_bodies, 3), dtype=np.float32)
+
+            if ctrl_pressed and lmb_pressed:
+                cam_p = np.array(camera.curr_position, dtype=np.float32)
+                cam_look = np.array(camera.curr_lookat, dtype=np.float32)
+                cam_up = np.array(camera.curr_up, dtype=np.float32)
+
+                if selected_body == -1:
+                    ray_dir = get_camera_ray_dir(
+                        mouse_pos[0],
+                        mouse_pos[1],
+                        cam_p,
+                        cam_look,
+                        cam_up,
+                        45.0,
+                        1280.0 / 720.0,
+                    )
+                    min_t = np.inf
+                    pos, _, rot, _ = self.scene.get_state()
+                    for i in range(self.n_bodies):
+                        orig_l = rot[i].T @ (cam_p - pos[i])
+                        dir_l = rot[i].T @ ray_dir
+                        hit, t, n_l = self.scene.bodies[i].ray_intersect(orig_l, dir_l)
+                        if hit and t < min_t:
+                            min_t = t
+                            selected_body = i
+                            click_pos_w = cam_p + t * ray_dir
+                            click_normal_w = rot[i] @ n_l
+                    if selected_body != -1:
+                        original_mouse_pos = mouse_pos
+
+                if selected_body != -1:
+                    curr_ray_dir = get_camera_ray_dir(
+                        mouse_pos[0],
+                        mouse_pos[1],
+                        cam_p,
+                        cam_look,
+                        cam_up,
+                        45.0,
+                        1280.0 / 720.0,
+                    )
+                    orig_ray_dir = get_camera_ray_dir(
+                        original_mouse_pos[0],
+                        original_mouse_pos[1],
+                        cam_p,
+                        cam_look,
+                        cam_up,
+                        45.0,
+                        1280.0 / 720.0,
+                    )
+
+                    V_drag = (curr_ray_dir - orig_ray_dir) * 500.0
+                    N = click_normal_w
+                    V_tangent = V_drag - np.dot(V_drag, N) * N
+
+                    F = V_tangent * 20.0
+
+                    pos, _, _, _ = self.scene.get_state()
+                    r = click_pos_w - pos[selected_body]
+                    tau = np.cross(r, F)
+
+                    applied_forces[selected_body] = F
+                    applied_torques[selected_body] = tau
+            else:
+                selected_body = -1
+
+            self.step(applied_forces, applied_torques)
             self.render(window, camera, canvas, scene_3d)
             frame += 1
 
--- lab1/util.py
+++ lab1/util.py
@@ -19,3 +19,61 @@ def skew_symmetric(v):
         [[0.0, -v[2], v[1]], [v[2], 0.0, -v[0]], [-v[1], v[0], 0.0]],
         dtype=np.float32,
     )
+
+
+def get_camera_ray_dir(u, v, cam_pos, cam_lookat, cam_up, fov_deg=45.0, aspect=1.0):
+    F = np.array(cam_lookat, dtype=np.float32) - np.array(cam_pos, dtype=np.float32)
+    F /= np.linalg.norm(F) + 1e-8
+    U0 = np.array(cam_up, dtype=np.float32)
+    U0 /= np.linalg.norm(U0) + 1e-8
+    R = np.cross(F, U0)
+    R /= np.linalg.norm(R) + 1e-8
+    U = np.cross(R, F)
+    U /= np.linalg.norm(U) + 1e-8
+
+    fov_rad = np.radians(fov_deg)
+    tan_half_fov = np.tan(fov_rad / 2.0)
+
+    x_ndc = 2.0 * u - 1.0
+    y_ndc = 2.0 * v - 1.0
+
+    dir = F + R * (x_ndc * tan_half_fov * aspect) + U * (y_ndc * tan_half_fov)
+    return dir / np.linalg.norm(dir)
+
+
+def ray_aabb_intersect(orig, dir, half_ext):
+    t_min = -np.inf
+    t_max = np.inf
+    hit_normal = np.zeros(3, dtype=np.float32)
+
+    for i in range(3):
+        if np.abs(dir[i]) < 1e-8:
+            if orig[i] < -half_ext[i] or orig[i] > half_ext[i]:
+                return False, 0.0, hit_normal
+        else:
+            inv_d = 1.0 / dir[i]
+            t0 = (-half_ext[i] - orig[i]) * inv_d
+            t1 = (half_ext[i] - orig[i]) * inv_d
+            n0 = np.zeros(3, dtype=np.float32)
+            n0[i] = -1.0
+            n1 = np.zeros(3, dtype=np.float32)
+            n1[i] = 1.0
+
+            if t0 > t1:
+                t0, t1 = t1, t0
+                n0, n1 = n1, n0
+
+            if t0 > t_min:
+                t_min = t0
+                hit_normal = n0
+
+            if t1 < t_max:
+                t_max = t1
+
+            if t_max < t_min:
+                return False, 0.0, np.zeros(3)
+
+    if t_max < 0.0:
+        return False, 0.0, np.zeros(3)
+
+    return True, t_min if t_min > 0 else t_max, hit_normal
